466c4033dc314be22991366301d30050a1493bd3
Anastasia Han <anastasia.han@yahoo.com>
2022-02-11 21:41:23 -0500

    Console Lines added

---
+++ PierreBakery/Program.cs
@@ -0,0 +1,13 @@
+using System;
+using PierreBakery.Models;
+
+namespace PierreBakeryUI
+{
+  public class Program
+  {
+    public static void Main()
+    Console.WriteLine("Welcome to the World Famous Pierre's Bakery! 10 Year ZAGAT / 3 Star Michelin");
+    Console.WriteLine(".:*~*:._.:*~*:._.:*~*:._.:*~*:._.:*~*:._.:*~*:._.:*~*:._.:*~*:.:*~*:._.:*~*:.");
+
+  }
+}
