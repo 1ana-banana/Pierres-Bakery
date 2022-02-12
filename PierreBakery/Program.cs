b5a13ae184d443b654d596ae1a653c7b42491336
Anastasia Han <anastasia.han@yahoo.com>
2022-02-11 22:05:23 -0500

    Added more console lines

--- PierreBakery/Program.cs
+++ PierreBakery/Program.cs
@@ -6,8 +6,22 @@ namespace PierreBakeryUI
   public class Program
   {
     public static void Main()
-    Console.WriteLine("Welcome to the World Famous Pierre's Bakery! 10 Year ZAGAT / 3 Star Michelin");
-    Console.WriteLine(".:*~*:._.:*~*:._.:*~*:._.:*~*:._.:*~*:._.:*~*:._.:*~*:._.:*~*:.:*~*:._.:*~*:.");
+    {
+      Console.WriteLine("Welcome to the World Famous Pierre's Bakery! 10 Year ZAGAT / 3 Star Michelin");
+      Console.WriteLine(".:*~*:._.:*~*:._.:*~*:._.:*~*:._.:*~*:._.:*~*:._.:*~*:._.:*~*:.:*~*:._.:*~*:.");
+      Console.WriteLine("Press Enter to see what we have for you today! Prepare to wipe down your drool!");
+      Console.ReadLine();
+      Console.WriteLine("House-Secret TangZhong Milk Bread Loaf: $12 / Chives and Cheese Quiche: $5");
+      Console.WriteLine("Press Enter to see today's Baker Special!");
+      Console.ReadLine();
+      Console.WriteLine("Specials:");
+      Console.WriteLine(".:*~*:._.:*~*:._.:*~*:._.:*~*");
+      Console.WriteLine("Milk Bread: Buy 2 Loaves, get one FREE");
+      Console.WriteLine("Buy 3 Quiche for $13");
+      Console.WriteLine("Enter your order, Type [Bread] or [Quiche]");
+      
+    }
+
 
   }
 }
