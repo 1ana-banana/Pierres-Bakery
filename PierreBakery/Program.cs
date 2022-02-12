c1c2a36bfeafe10d0367417181091653ad0b5316
Anastasia Han <anastasia.han@yahoo.com>
2022-02-11 22:35:17 -0500

    Completed UI Logic

--- PierreBakery/Program.cs
+++ PierreBakery/Program.cs
@@ -19,9 +19,46 @@ namespace PierreBakeryUI
       Console.WriteLine("Milk Bread: Buy 2 Loaves, get one FREE");
       Console.WriteLine("Buy 3 Quiche for $13");
       Console.WriteLine("Enter your order, Type [Bread] or [Quiche]");
-      
+      string order = Console.ReadLine().ToLower();
+      Console.WriteLine("We have your order: " + " " + order + "Please enter the quantity you would like to purchase.");
+      int quantity = int.Parse(Console.ReadLine());
+      if (quantity == 1 && order == "bread") {
+        Bread newBread = new Bread(12, 1);
+        Console.WriteLine("Thank you for ordering!, the total today comes out to" + " $" + newBread.GetPrice());
+      } 
+      else if (quantity == 2 && order == "bread") 
+      {
+        Console.WriteLine("Thank you for ordering!, the total today comes out to $5.");
+      } 
+      else if(quantity >= 3 && order == "bread") 
+      {
+        Bread newBread = new Bread(5, quantity);
+        Console.WriteLine("Thank you for ordering!, the total today comes out to" + " $" + newBread.GetDiscount());
+      }
+      else if (quantity == 1 && order == "quiche")
+      { 
+        Quiche newQuiche = new Quiche(5, 1);
+        Console.WriteLine("Thank you for ordering!, the total today comes out to" + " $" + newPastry.GetPrice());
+      }
+      else if (quantity == 2 && order == "quiche") 
+      {
+        Quiche newQuiche = new Quiche(5, 2);
+        Console.WriteLine("Thank you for ordering!, the total today comes out to" + " $" + newPastry.GetPrice());
+      }
+      else if (quantity == 3 && order == "quiche")
+      {
+        Console.WriteLine("Thank you for ordering!, the total today comes out to $5.");
+      }
+      else if (quantity > 3 && order == "quiche")
+      {
+        Quiche newQuiche = new Quiche(2, quantity);
+        Console.WriteLine("Thank you for ordering!, the total today comes out to" + " $" + newPastry.GetDiscount());
+      } 
+      else 
+      {
+        Console.WriteLine("It looks like there was a problem. Please try again!");
+      }
+      Console.ReadLine();
     }
-
-
   }
 }
