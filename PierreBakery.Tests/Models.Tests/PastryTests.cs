a3d8ed746d6365c032d5035a7157f22e7171e33d
Anastasia Han <anastasia.han@yahoo.com>
2022-02-12 18:38:20 -0500

    All tests for both classes pass

--- PierreBakery.Tests/Models.Tests/PastryTests.cs
+++ PierreBakery.Tests/Models.Tests/PastryTests.cs
@@ -12,5 +12,21 @@ namespace PierreBakery.Tests
       Pastry newPastry = new Pastry(2,2);
       Assert.AreEqual(typeof(Pastry), newPastry.GetType());
     }
+    [TestMethod]
+    public void GetPastryPrice_ReturnPrice_Int()
+    {
+      Pastry newPastry = new Pastry(2, 1);
+      int result = 2;
+      int price = newPastry.GetPrice();
+      Assert.AreEqual(price, result);
+    }
+    [TestMethod]
+    public void GetPastryDiscount_ReturnPrice_Int()
+    {
+      Pastry newPastry = new Pastry(2,10);
+      int discount = newPastry.GetDiscount();
+      int result = 17;
+      Assert.AreEqual(result, discount);
+    }
   }
 }
