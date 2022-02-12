a96a216fca588e19b0a64c3284c7d87ac1386995
Anastasia Han <anastasia.han@yahoo.com>
2022-02-11 22:43:39 -0500

    Completed Bread functions

---
+++ PierreBakery/Models/Bread.cs
@@ -0,0 +1,26 @@
+using System;
+using PierreBakeryUI;
+
+namespace PierreBakery.Models
+{
+  public class Bread
+  {
+    public int Price {get; set;}
+    public int Quantity {get; set;}
+    public Bread(int price, int quantity)
+    {
+      Price = price;
+      Quantity = quantity;
+    }
+    public int GetPrice()
+    {
+      return Price * Quantity;
+    }
+    public int GetDiscount()
+    {
+      int freeAmount = Quantity/3;
+      int total = Quantity - freeAmount;
+      return Price * total;
+    }
+  }
+}
